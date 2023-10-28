a3bc27d924ed1e607066b325b16dada721357c61
Wyatt Winborne <wizzlefiz@gmail.com>
2023-10-28 09:55:50 -0700

    Crank to Move
    Added a small crank to move on update

--- source/main.lua
+++ source/main.lua
@@ -3,13 +3,18 @@ import "CoreLibs/graphics"
 import "CoreLibs/sprites"
 import "CoreLibs/timer"
 
+local pd <const> = playdate
 local gfx <const> = playdate.graphics
 
 local playerX, playerY = 200, 120
 local playerRadius = 10
+local playerSpeed = 3
 
-function playdate.update() 
-    print("Hello World")
+function pd.update() 
+    gfx.clear()
+    local crankAngle = math.rad(pd.getCrankPosition())
+    playerX += math.sin(crankAngle) * playerSpeed
+    playerY -= math.cos(crankAngle) * playerSpeed
     gfx.fillCircleAtPoint(playerX, playerY, playerRadius)
     
 end
