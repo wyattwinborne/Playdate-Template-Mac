8cc1d0b32db3f771bdf7b7861bffb03cb9e5756a
wyattwinborne <wizzlefiz@gmail.com>
2023-11-24 08:44:33 -0800

    cleaned up some remaining starter-game code

--- source/main.lua
+++ source/main.lua
@@ -2,39 +2,11 @@ import "CoreLibs/object"
 import "CoreLibs/graphics"
 import "CoreLibs/sprites"
 import "CoreLibs/timer"
-
-import "player"
-import "enemySpawner"
-import "scoreDisplay"
-import "screenShake"
 ------------------------------------------------------------
 local pd <const> = playdate
 local gfx <const> = pd.graphics
 ------------------------------------------------------------
-
-local screenShakeSprite = ScreenShake()
-
-function resetGame()
-    resetScore()
-    clearEnemies()
-    stopSpawner()
-    startSpawner()
-    setShakeAmount(10)
-end
-
-function setShakeAmount(amount)
-    screenShakeSprite:setShakeAmount(amount)
-end
-
-Player(30, 120)
-startSpawner()
-createScoreDisplay()
-
 function pd.update() 
     gfx.sprite.update()
     pd.timer.updateTimers()
-end
-
-function loseScreen()
-    
 end
